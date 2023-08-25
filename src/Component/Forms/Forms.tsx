914f2cfc49c7a24add8a7f297f2b79573dd136fd
Shehroz <mshehroz302@gmail.com>
2023-08-25 22:18:04 +0500

    Create simple form

--- src/Component/Forms/Forms.tsx
+++ src/Component/Forms/Forms.tsx
@@ -1,11 +1,68 @@
 const Forms = () => {
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="name"></label>
-        <input id="name" type="text" />
-      </div>
-    </form>
+    <div className="flex justify-center items-center h-screen bg-zinc-300">
+      <form className="bg-white shadow-md rounded-xl px-8 p-12 mb-4 w-full max-w-xl">
+        <div className="flex w-full justify-between mb-3">
+          <div className="w-[48%]">
+            <label
+              htmlFor="fname"
+              className="block text-gray-500 text-sm font-medium mb-2"
+            >
+              First Name
+            </label>
+            <input
+              id="fname"
+              type="text"
+              className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="w-[48%]">
+            <label
+              htmlFor="lname"
+              className="block text-gray-500 text-sm font-medium mb-2"
+            >
+              Last Name
+            </label>
+            <input
+              id="lname"
+              type="text"
+              className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+        </div>
+        <div className="mb-3">
+          <label
+            htmlFor="age"
+            className="block text-gray-500 text-sm font-medium mb-2"
+          >
+            Age
+          </label>
+          <input
+            id="age"
+            type="number"
+            className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            htmlFor="password"
+            className="block text-gray-500 text-sm font-medium mb-2"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex justify-center items-center mt-5">
+          <button className="bg-green-300 hover:bg-green-500 text-white font-normal py-2 px-4 rounded-full">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
