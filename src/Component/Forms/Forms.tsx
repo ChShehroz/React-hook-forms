02ab29efb50cd562891ae6c4d0d037aa8f76900d
Shehroz <mshehroz302@gmail.com>
2023-08-28 23:21:21 +0500

    Form Validation

--- src/Component/Forms/Forms.tsx
+++ src/Component/Forms/Forms.tsx
@@ -1,9 +1,21 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  firstName: string;
+  lastName: string;
+  age: number;
+  password: string;
+}
+
 const Forms = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
     <div className="flex justify-center items-center h-screen bg-zinc-300">
       <form
@@ -22,11 +34,20 @@ const Forms = () => {
               First Name
             </label>
             <input
-              {...register("firstName")}
+              {...register("firstName", {
+                required: "The First Name field is required",
+                minLength: {
+                  value: 3,
+                  message: "Must be 3 characters or more",
+                },
+              })}
               id="fname"
               type="text"
               className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+            {errors.firstName && (
+              <span className="text-red-800">{errors.firstName.message}</span>
+            )}
           </div>
           <div className="w-[48%]">
             <label
