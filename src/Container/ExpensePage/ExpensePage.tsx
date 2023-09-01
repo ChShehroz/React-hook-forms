ed4691b8229d7e52aeaa73dce34d783b4f899359
Shehroz <mshehroz302@gmail.com>
2023-09-01 22:28:04 +0500

    Set record page

--- src/Container/ExpensePage/ExpensePage.tsx
+++ src/Container/ExpensePage/ExpensePage.tsx
@@ -1,31 +1,43 @@
 import { useState } from "react";
 import ExpenseList from "../../Expense-tracker/Component/ExpenseList";
 import ExpenseFilter from "../../Expense-tracker/Component/ExpenseFilter";
+import ExpenseForm from "../../Expense-tracker/Component/ExpenseForm"; // Assuming you have an ExpenseForm component
 
 const ExpensePage = () => {
   const [selectedCategory, setSelectedCategory] = useState("All categories");
-
   const [expense, setExpense] = useState([
-    { id: 1, description: "aaa", amount: 100, category: "Groceries" },
-    { id: 2, description: "bbb", amount: 100, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 100, category: "Entertainment" },
-    { id: 4, description: "ddd", amount: 100, category: "Utilities" },
+    { id: 1, description: "Lux Soap", amount: 100, category: "Groceries" },
+    { id: 2, description: "Electricity", amount: 80, category: "Utilities" },
+    { id: 3, description: "GTA 5", amount: 56, category: "Entertainment" },
+    { id: 4, description: "Meat", amount: 100, category: "Groceries" },
   ]);
+
   const visibleExpense =
     selectedCategory === "All categories"
       ? expense
       : expense.filter((e) => e.category === selectedCategory);
 
   return (
-    <div className="my-12">
-      <ExpenseFilter
-        onSelectCategory={(category) => setSelectedCategory(category)}
-      />
+    <div className="my-12 flex">
+      {/* Sidebar for the form */}
+      <div className="w-1/4 p-4  bg-transparent">
+        <ExpenseForm
+          onSubmit={(formData) =>
+            setExpense([...expense, { ...formData, id: expense.length + 1 }])
+          }
+        />
+      </div>
 
-      <ExpenseList
-        expenses={visibleExpense}
-        onDelete={(id) => setExpense(expense.filter((e) => e.id != id))}
-      />
+      {/* Main content */}
+      <div className="w-3/4 p-4">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+        <ExpenseList
+          expenses={visibleExpense}
+          onDelete={(id) => setExpense(expense.filter((e) => e.id !== id))}
+        />
+      </div>
     </div>
   );
 };
--- src/Expense-tracker/Component/ExpenseFilter.tsx
+++ src/Expense-tracker/Component/ExpenseFilter.tsx
@@ -8,7 +8,7 @@ const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
     <div className="flex justify-center items-center">
       <select
-        className="min-w-max max-w-2xl w-full p-4 mb-5 rounded-3xl outline-none transition duration-150 ease-in-out bg-white shadow-lg text-sm focus:ring-2 focus:ring-blue-300"
+        className="min-w-max max-w-2xl w-full p-4 mb-5 rounded-3xl outline-none transition duration-150 ease-in-out bg-white shadow-lg text-sm "
         onChange={(event) => onSelectCategory(event.target.value)}
       >
         <option>All categories</option>
